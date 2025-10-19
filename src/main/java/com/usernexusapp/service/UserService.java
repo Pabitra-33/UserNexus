e3a7c220ce3f8a323e2c0761f981318bf593d6f1
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-19 16:38:57 +0530

    updated the service interface with a new method and leave the repository interface as it is

--- src/main/java/com/usernexusapp/service/UserService.java
+++ src/main/java/com/usernexusapp/service/UserService.java
@@ -1,5 +1,19 @@
 package com.usernexusapp.service;
 
-public class UserService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.usernexusapp.entity.User;
+
+@Service
+public interface UserService {
+	
+	//list of abstract methods used for CRUD operations
+	public User createUser(User newUser);
+	public List<User> readAllUsers();
+	public Optional<User> getUserById(Integer id);
+	public User updateUser(Integer id, User updateUser);
+	public boolean deleteUser(Integer id);
 }
