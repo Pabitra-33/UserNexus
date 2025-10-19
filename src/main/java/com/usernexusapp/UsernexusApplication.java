8b1f8bbe3bc0fe480710a03647a07e1f5d88c14a
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-19 11:30:40 +0530

    project setup

---
+++ src/main/java/com/usernexusapp/UsernexusApplication.java
@@ -0,0 +1,13 @@
+package com.usernexusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsernexusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsernexusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usernexusapp/UsernexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usernexusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsernexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
