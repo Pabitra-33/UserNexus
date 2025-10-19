8ce8c2d2d114878db64780e867b7b54b6c0a556f
Pabitra-33 <kumarpabitra345@gmail.com>
2025-10-19 16:34:38 +0530

    this is my entity class which maps to the database table

--- src/main/java/com/usernexusapp/entity/User.java
+++ src/main/java/com/usernexusapp/entity/User.java
@@ -1,10 +1,46 @@
 package com.usernexusapp.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
-
+	
+	//attributes of user
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer uId;
+	private String name;
+	private String address;
+	private Long mobNo;
+	
+	//getters and setters
+	public Integer getuId() {
+		return uId;
+	}
+	public void setuId(Integer uId) {
+		this.uId = uId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Long getMobNo() {
+		return mobNo;
+	}
+	public void setMobNo(Long mobNo) {
+		this.mobNo = mobNo;
+	}
 }
